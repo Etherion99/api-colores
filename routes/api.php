165132cf1f183ae9a06f44a140e43d9562e06419
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2022-04-21 12:34:06 -0500

    xml delete

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,3 @@ use App\Http\Controllers\ColorController;
 use App\Http\Controllers\ColorXMLController;
 
 Route::resource('colors', ColorController::class);
-Route::resource('colors_xml', ColorXMLController::class);
