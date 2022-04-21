7d0455c75269baef13ff77853a4a970dd64e0eed
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2022-04-21 12:00:13 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ColorController;
+use App\Http\Controllers\ColorXMLController;
 
 Route::resource('colors', ColorController::class);
-Route::resource('colors/xml', ColorController::class);
+Route::resource('colors/xml', ColorXMLController::class);
